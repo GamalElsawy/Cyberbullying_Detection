719c775413c74147dc5f5bd691c1547025e8e8f4
beso <your_email@example.com>
2021-05-02 01:00:41 +0200

    About Completed

---
+++ src/app/Model/Account.ts
@@ -0,0 +1,8 @@
+export class Account {
+  Name:String;
+  Image:String;
+  EMail:String;
+  Password:String;
+  Phone:String;
+  Address:String;
+}
--- src/app/info/info.component.ts
+++ src/app/info/info.component.ts
@@ -1,12 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Account } from '../Model/Account';
 @Component({
   selector: 'app-info',
   templateUrl: './info.component.html',
   styleUrls: ['./info.component.css']
 })
+
 export class InfoComponent implements OnInit {
 
+  acc : Account = {
+    Name : "Bassam",
+    Address:"62 elmaadi cairo",
+    EMail:"BassamSaber@gmail.com",
+    Image:"http://simpleicon.com/wp-content/uploads/user1.png",
+    Password:"111111",
+    Phone:"01141248413"
+  };
+
+  opened=false;
+  
   constructor() { }
 
   ngOnInit(): void {
